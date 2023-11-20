f2f27fbf98219550779a99f6987e499cc2bde90f
epatrizio <epatrizio@mpns.fr>
2023-11-20 20:39:43 +0100

    more typing tests

--- test/typer.lua
+++ test/typer.lua
@@ -1,11 +1,12 @@
--- print(not 10)   -- ok todo
--- print(not 10.5) -- ok todo
--- print(not nil)  -- ok todo
+print(not 10)   -- ok
+print(not 10.5) -- ok
+print(not nil)  -- ok
 
 -- print(10+false) -- ko
 -- print(false+10) -- ko
 -- print(10+nil)   -- ko
 -- print(10+"str") -- ko
+-- print(1+"str")  -- ko
 
 -- print(-nil)     -- ko
 -- print(-false)   -- ko
@@ -15,6 +16,7 @@
 -- print(true//1)  -- ko
 -- print("str"//1) -- ko
 -- print(nil%1)    -- ko
+-- print(nil%"2.") -- ko
 -- print(true%1)   -- ko
 -- print("str"%1)  -- ko
 
@@ -32,13 +34,16 @@
 -- print(~"str")   -- ko
 -- print(1&1.5)    -- ok > interpretation error (1.5 not integer - 1&1.0 ok)
 
--- print(1 < true) -- ko todo
--- print(1 < nil)  -- ko todo
+-- print(1 < true)      -- ko
+-- print(1 < nil)       -- ko
+-- print(nil < nil)     -- ko
+-- print(true > false)  -- ko
+-- print("str" >= 42.42)-- ko
 
--- print(10 and 20)-- ok todo
--- print(20 and 10)-- ok todo
--- print(10 or 20) -- ok todo
--- print(20 or 10) -- ok todo
+print(10 and 20)    -- ok
+print(20 and 10)    -- ok
+print(10 or 20)     -- ok
+print(20 or 10)     -- ok
 
 -- print(#nil)    -- ko
 -- print(#42)     -- ko
@@ -46,22 +51,21 @@
 -- print(#true)   -- ko
 -- print(#false)  -- ko
 
--- print(1 .. "str")       -- ok 1str
--- print(1.1 .. "str")     -- ok 1.1str
+print(1 .. "str")       -- ok 1str
+print(1.1 .. "str")     -- ok 1.1str
 -- print(nil .. "str")  -- ko
 -- print(true .. "str") -- ko
 
 if false then print(0)
 elseif false then print(0)
 elseif true then
-    a = 2
-    b = 40
-    print(a+b)
-    b = false
-    print(a+b)
+  a = 2
+  b = 40
+  print(a+b)
+  b = false
+  print(a+b)
 else print(0) end
 
--- for i = nil,10,1
---     do
---         print(i)
---     end
+-- for i = nil,10,1 do      -- ko (todo)
+--   print(i)
+-- end
--- test/typer.t
+++ test/typer.t
@@ -1,5 +1,14 @@
 typer checks:
   $ dune exec ola typer.lua
   interprete ...
+  false
+  false
+  true
+  20
+  10
+  10
+  20
+  1str
+  1.1str
   42
-  Interpretation error: File "typer.lua", line 61, char 12: Typing error: attempt to perform arithmetic on a boolean value
+  Interpretation error: File "typer.lua", line 66, char 10: Typing error: attempt to perform arithmetic on a boolean value
