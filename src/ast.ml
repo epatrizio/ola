6654224bfb4e7f2ecf94d664454795f53a5f09d9
Eric Patrizio <epatrizio@mpns.fr>
2025-08-24 09:39:23 +0200

    refacto: issue #2 improve pretty printer boxes on blocks (wip)

--- src/ast.ml
+++ src/ast.ml
@@ -133,7 +133,7 @@ open Format
 
 let pp_sep fmt () = fprintf fmt ", "
 
-let print_attrib fmt attrib = fprintf fmt "<%s>" attrib
+let print_attrib fmt attrib = fprintf fmt {|<%s>|} attrib
 
 let print_unop fmt unop =
   pp_print_string fmt
@@ -182,55 +182,55 @@ let rec print_value fmt value =
   | Vnumber num -> print_number fmt num
   | Vstring s -> fprintf fmt {|"%a"|} pp_print_string s
   | Vfunction (i, _, _) | VfunctionStdLib (i, _) ->
-    fprintf fmt "function: %a" pp_print_int (Int32.to_int i)
+    fprintf fmt {|function: %a|} pp_print_int (Int32.to_int i)
   | VfunctionReturn vl -> (pp_print_list ~pp_sep print_value) fmt vl
-  | Vtable (i, _flo) -> fprintf fmt "table: %a" pp_print_int (Int32.to_int i)
+  | Vtable (i, _flo) -> fprintf fmt {|table: %a|} pp_print_int (Int32.to_int i)
 
 let rec print_parlist fmt pl =
   match pl with
   | PLlist (nl, eo) ->
-    fprintf fmt "%a%a" (pp_print_list ~pp_sep pp_print_string) nl print_eo eo
+    fprintf fmt {|%a%a|} (pp_print_list ~pp_sep pp_print_string) nl print_eo eo
   | PLvariadic e -> print_expr fmt e
 
 and print_var fmt v =
   match v with
   | VarName n -> pp_print_string fmt n
   | VarTableField (pexp, exp) ->
-    fprintf fmt "%a[%a]" print_prefixexp pexp print_expr exp
-(* | VarTableFieldName (pexp, n) -> fprintf fmt "%a.%s" print_prefixexp pexp n *)
+    fprintf fmt {|%a[%a]|} print_prefixexp pexp print_expr exp
+(* | VarTableFieldName (pexp, n) -> fprintf fmt {|%a.%s|} print_prefixexp pexp n *)
 
 and print_field fmt f =
   match f with
   | Fexp e -> print_expr fmt e
-  | Fname (n, e) -> fprintf fmt "%s = %a" n print_expr e
-  | Fcol (e1, e2) -> fprintf fmt "[%a] = %a" print_expr e1 print_expr e2
+  | Fname (n, e) -> fprintf fmt {|%s = %a|} n print_expr e
+  | Fcol (e1, e2) -> fprintf fmt {|[%a] = %a|} print_expr e1 print_expr e2
 
 and print_fieldlist fmt fl =
-  fprintf fmt "{%a}" (pp_print_list ~pp_sep print_field) fl
+  fprintf fmt {|{%a}|} (pp_print_list ~pp_sep print_field) fl
 
-and print_eo fmt eo = Option.iter (fprintf fmt ", %a" print_expr) eo
+and print_eo fmt eo = Option.iter (fprintf fmt {|, %a|} print_expr) eo
 
 and print_funcbody fmt (pl, b) =
-  fprintf fmt "(%a)@.%a@.end@." print_parlist pl print_block b
+  fprintf fmt {|(%a)@,%aend|} print_parlist pl print_block b
 
 and print_prefixexp fmt prexp =
   match prexp with
   | PEvar v -> print_var fmt v
   | PEfunctioncall fc -> print_functioncall fmt fc
-  | PEexp e -> fprintf fmt "(%a)" print_expr e
+  | PEexp e -> fprintf fmt {|(%a)|} print_expr e
 
 and print_args fmt args =
   match args with
-  | Aexpl el -> fprintf fmt "(%a)" (pp_print_list ~pp_sep print_expr) el
+  | Aexpl el -> fprintf fmt {|(%a)|} (pp_print_list ~pp_sep print_expr) el
   | Atable fl -> print_fieldlist fmt fl
 (* | Astr s -> pp_print_string fmt s *)
 
 and print_functioncall fmt fc =
   match fc with
   | FCpreargs (pexp, args) ->
-    fprintf fmt "%a%a" print_prefixexp pexp print_args args
+    fprintf fmt {|%a%a|} print_prefixexp pexp print_args args
   | FCprename (pexp, n, args) ->
-    fprintf fmt "%a:%s(%a)" print_prefixexp pexp n print_args args
+    fprintf fmt {|%a:%s(%a)|} print_prefixexp pexp n print_args args
 
 and print_expr fmt (_loc, expr) =
   match expr with
@@ -243,68 +243,68 @@ and print_expr fmt (_loc, expr) =
     print_binop fmt bop;
     print_expr fmt e2
   | Evariadic -> pp_print_string fmt "..."
-  | Efunctiondef fb -> fprintf fmt "function %a" print_funcbody fb
+  | Efunctiondef fb -> fprintf fmt {|function %a|} print_funcbody fb
   | Eprefix prexp -> print_prefixexp fmt prexp
   | Etableconstructor fl -> print_fieldlist fmt fl
 
 and print_stmt fmt stmt =
   let pp_name_attrib fmt (name, attrib_opt) =
     pp_print_string fmt name;
-    Option.iter (fprintf fmt " %a " print_attrib) attrib_opt
+    Option.iter (fprintf fmt {| %a |} print_attrib) attrib_opt
   in
   match stmt with
   | Sempty -> fprintf fmt ""
   | Sassign (vl, lel) ->
-    fprintf fmt "%a = %a@."
+    fprintf fmt {|%a = %a@,|}
       (pp_print_list ~pp_sep print_var)
       vl
       (pp_print_list ~pp_sep print_expr)
       lel
   | SassignLocal (nal, el) ->
-    fprintf fmt "local %a%a%a@."
+    fprintf fmt {|local %a%a%a@,|}
       (pp_print_list ~pp_sep pp_name_attrib)
       nal pp_print_string
       (if List.length el > 0 then " = " else "")
       (pp_print_list ~pp_sep print_expr)
       el
-  | Sbreak -> fprintf fmt "break@."
+  | Sbreak -> fprintf fmt {|break@,|}
   | Sreturn el ->
-    fprintf fmt "return %a@." (pp_print_list ~pp_sep print_expr) el
-  | Slabel n -> fprintf fmt "::%s::@." n
-  | Sgoto n -> fprintf fmt "goto %s@." n
-  | Sblock b -> fprintf fmt "do@.@[<v>%a@]end@." print_block b
+    fprintf fmt {|return %a@,|} (pp_print_list ~pp_sep print_expr) el
+  | Slabel n -> fprintf fmt {|::%s::@,|} n
+  | Sgoto n -> fprintf fmt {|goto %s@,|} n
+  | Sblock b -> fprintf fmt {|do@,%aend@,|} print_block b
   | Swhile (e, b) ->
-    fprintf fmt "while %a do@.@[<v>%a@]end@." print_expr e print_block b
+    fprintf fmt {|while %a do@,%aend@,|} print_expr e print_block b
   | Srepeat (b, e) ->
-    fprintf fmt "repeat@.@[<v>%a@]until %a@." print_block b print_expr e
+    fprintf fmt {|repeat@,%auntil %a@,|} print_block b print_expr e
   | Sif (e, b, lebl, ob) ->
     let print_elseif fmt (e, b) =
-      fprintf fmt "elseif %a then %a@." print_expr e print_block b
+      fprintf fmt {|elseif %a then@,%a|} print_expr e print_block b
     in
-    fprintf fmt "if %a then %a %a" print_expr e print_block b
+    fprintf fmt {|if %a then@,%a%a|} print_expr e print_block b
       (pp_print_list print_elseif)
       lebl;
-    Option.iter (fprintf fmt "else %a " print_block) ob;
-    fprintf fmt "end@."
+    Option.iter (fprintf fmt {|else@,%a|} print_block) ob;
+    fprintf fmt "end@,"
   | Sfor (n, e1, e2, oe, b) ->
-    fprintf fmt "for %a = %a, %a%a do@.@[<v>%a@]end@." pp_print_string n
-      print_expr e1 print_expr e2 print_eo oe print_block b
+    fprintf fmt {|for %a = %a, %a%a do@,%aend@,|} pp_print_string n print_expr
+      e1 print_expr e2 print_eo oe print_block b
   | Siterator (nl, lel, b) ->
-    fprintf fmt "for %a = %a do@.@[<v>%a@]end@."
+    fprintf fmt {|for %a = %a do@,%aend@,|}
       (pp_print_list ~pp_sep pp_print_string)
       nl
       (pp_print_list ~pp_sep print_expr)
       lel print_block b
   (* | Sfunction (fname, fbody) ->
-     fprintf fmt "function %a%a" print_funcname fname print_funcbody fbody *)
+     fprintf fmt {|function %a%a|} print_funcname fname print_funcbody fbody *)
   (* | SfunctionLocal (name, fbody) ->
-     fprintf fmt "local function %a%a" print_var (Name name)
+     fprintf fmt {|local function %a%a|} print_var (Name name)
        print_funcbody fbody *)
   | SfunctionCall fc -> print_functioncall fmt fc
 
 and print_block fmt block =
-  let pp_sep _fmt () = () in
-  pp_print_list ~pp_sep print_stmt fmt block
+  let pp_sep fmt () = fprintf fmt "@," in
+  fprintf fmt {|@[<v 2>%a@]|} (pp_print_list ~pp_sep print_stmt) block
 
 let pp_loc fmt (loc : location) =
   let start, _end = loc in
