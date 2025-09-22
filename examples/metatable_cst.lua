d53ab2939cfcb8c99fa787400e4bda7b1c450c0f
Eric Patrizio <epatrizio@mpns.fr>
2025-09-22 15:46:28 +0200

    add metatable example: table of constants

---
+++ examples/metatable_cst.lua
@@ -0,0 +1,73 @@
+-- classic meta-mechanism application: table of constants
+
+-- 1. table created once at the beginning and it is impossible to add new constants after
+
+print("1. -----")
+
+function to_constants(tbl)
+  return setmetatable({}, {
+    __index = tbl,
+    __newindex = function(t, key, value)
+        print("attempt to change constants", key, value)
+      end
+    })
+end
+
+CST = {11, 22, my_cst = "my_cst_value"}   -- constants init
+CST = to_constants(CST)
+
+print(CST[1])         -- 11
+print(CST[2])         -- 22
+print(CST.my_cst)     -- my_cst_value
+
+CST[1] = 111                  -- KO: attempt to change constants
+CST[2] = 222                  -- KO: attempt to change constants
+CST.my_cst = "new_value"      -- KO: attempt to change constants
+
+CST[3] = 33                   -- KO: same with new value
+CST.my_new_cst = "new_value"  -- KO: same with new value
+
+print(CST[3])           -- nil
+print(CST.my_new_cst)   -- nil
+
+-- 2. Constants can be created as needed
+--    (experimental implementation. Idea: use a function to add constants that updates the metatable each time)
+
+print("2. -----")
+
+function add_const(key_cst, val_cst, tbl)
+  tbl_mt = getmetatable(tbl)
+  if tbl_mt ~= nil then
+    tbl = tbl_mt.__index
+  end
+  tbl[key_cst] = val_cst
+  return setmetatable({}, {
+    __index = tbl,
+    __newindex = function(t, key, value)
+        print("attempt to change constant directly", key, value)
+      end
+    })
+end
+
+CST = {}
+
+CST = add_const(1, 11, CST)
+CST = add_const(2, 22, CST)
+CST = add_const("my_cst", "my_cst_value", CST)
+
+print(CST[1])         -- 11
+print(CST[2])         -- 22
+print(CST.my_cst)     -- my_cst_value
+
+CST[1] = 111                  -- KO: attempt to change constant directly
+CST[2] = 222                  -- KO: attempt to change constant directly
+CST.my_cst = "new_value"      -- KO: attempt to change constant directly
+
+CST[3] = 33                   -- KO: Warning, for new value, add_const fonction
+CST.my_new_cst = "new_value"  -- KO: Warning, for new value, add_const fonction
+
+CST = add_const(3, 33, CST)
+CST = add_const("my_new_cst", "new_value", CST)
+
+print(CST[3])           -- 33
+print(CST.my_new_cst)   -- new_value
