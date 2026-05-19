ef396541685499050293c738516ab9ad30e852fb
Eric Patrizio <epatrizio@mpns.fr>
2026-05-19 16:39:28 +0200

    wip: luatable helpers experiments, better error management (result)

--- examples/ocaml/main.ml
+++ examples/ocaml/main.ml
@@ -6,11 +6,18 @@ open Ola.Ast
 open Syntax
 
 (* WIP: LuaTable helpers (create a specific module ?) *)
+
+let val_of_val_key key tbl =
+  match LuaTable.get key tbl with
+  | Ok v -> Ok v
+  | Error (Vnil ()) -> Error (None, "attempt to access a non-existent table key")
+  | Error v -> Ok v (* metatable_field "__index" *)
+
 let str_of_str_key key tbl =
-  match LuaTable.get (Vstring key) tbl with
-  | Ok (Vstring str) -> str
-  (* | Error _ TODO: check LuaTable.get impl > metatable_field "__index" *)
-  | _ -> assert false (* TODO: error ctrl *)
+  match val_of_val_key (Vstring key) tbl with
+  | Ok (Vstring str) -> Ok str
+  | Ok _ -> Error (None, "wrong value type, string expected")
+  | Error (l, msg) -> Error (l, msg ^ key)
 
 (* let int_of_str_key key tbl =
   match LuaTable.get (Vstring key) tbl with
@@ -18,14 +25,16 @@ let str_of_str_key key tbl =
   | _ -> assert false *)
 
 let tbl_of_str_key key tbl =
-  match LuaTable.get (Vstring key) tbl with
-  | Ok (Vtable tbl) -> tbl
-  | _ -> assert false
+  match val_of_val_key (Vstring key) tbl with
+  | Ok (Vtable tbl) -> Ok tbl
+  | Ok _ -> Error (None, "wrong value type, table expected")
+  | Error (l, msg) -> Error (l, msg ^ key)
 
 let int_of_int_key key tbl =
-  match LuaTable.get (Vnumber (Ninteger key)) tbl with
-  | Ok (Vnumber (Ninteger i)) -> i
-  | _ -> assert false
+  match val_of_val_key (Vnumber (Ninteger key)) tbl with
+  | Ok (Vnumber (Ninteger i)) -> Ok i
+  | Ok _ -> Error (None, "wrong value type, int expected")
+  | Error (l, msg) -> Error (l, msg ^ string_of_int key)
 
 (* **** *)
 
@@ -39,10 +48,9 @@ let () =
 
     (* let* data = Env.get_value "v0" env in *)
     (* WARNING! Limitation: we only have var names after scoping *)
-
     begin match vl with
     | Vtable tbl :: _ ->
-      let version = str_of_str_key "__version" tbl in
+      let* version = str_of_str_key "__version" tbl in
       print_endline version;
       begin match LuaTable.get (Vstring "hello") tbl with
       | Ok (Vfunction (_, _, _) as fct) ->
@@ -55,8 +63,8 @@ let () =
       | Ok _ -> assert false
       | Error _v -> assert false (* TODO *)
       end;
-      let tbl = tbl_of_str_key "data" tbl in
-      let i1 = int_of_int_key 1 tbl in
+      let* tbl = tbl_of_str_key "data" tbl in
+      let* i1 = int_of_int_key 1 tbl in
       print_int i1;
       print_newline ();
       Ok ()
